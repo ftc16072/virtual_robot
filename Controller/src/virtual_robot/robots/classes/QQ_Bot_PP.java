88771d55c2ffd97f1d6eec9a43edc81f408d7879
Alan Smith <alan@randomsmiths.com>
2022-12-20 19:09:00 -0500

    Update with configuration we used for scrimmage (and are planning for competition)

--- Controller/src/virtual_robot/robots/classes/QQ_Bot_PP.java
+++ Controller/src/virtual_robot/robots/classes/QQ_Bot_PP.java
@@ -1,7 +1,12 @@
 package virtual_robot.robots.classes;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorExImpl;
 import com.qualcomm.robotcore.hardware.DcMotorImpl;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DigitalChannelImpl;
+import com.qualcomm.robotcore.hardware.PassiveColorSensorImpl;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoImpl;
 import com.qualcomm.robotcore.hardware.configuration.MotorType;
 
@@ -18,9 +23,13 @@ import virtual_robot.controller.VirtualRobotController;
 @BotConfig(name = "QQ Bot PP", filename = "qq_bot_pp")
 public class QQ_Bot_PP extends MecanumPhysicsBase {
     private ServoImpl claw_servo = null;
+    private ServoImpl pulley_servo = null;
+
     private ServoImpl rotate_servo = null;
 
-    private DcMotorExImpl lift_motor = null;
+    private DcMotorExImpl right_lift_motor = null;
+    private DcMotorExImpl left_lift_motor = null;
+    private DigitalChannelImpl lift_switch = null;
     private VirtualRobotController.ColorSensorImpl coneDetector = null;
 
 
@@ -31,18 +40,26 @@ public class QQ_Bot_PP extends MecanumPhysicsBase {
     public void initialize() {
         super.initialize();
         hardwareMap.setActive(true);
-        claw_servo = (ServoImpl) hardwareMap.servo.get("claw");
-        lift_motor = (DcMotorExImpl) hardwareMap.dcMotor.get("lift_motor");
-        coneDetector = (VirtualRobotController.ColorSensorImpl) hardwareMap.colorSensor.get("cone_detector");
 
+        claw_servo = (ServoImpl) hardwareMap.servo.get("claw");
+        pulley_servo = (ServoImpl) hardwareMap.servo.get("horizontal");
+
+        right_lift_motor = (DcMotorExImpl) hardwareMap.dcMotor.get("right_lift_motor");
+        left_lift_motor = (DcMotorExImpl) hardwareMap.dcMotor.get("left_lift_motor");
+        lift_switch = (DigitalChannelImpl) hardwareMap.get(DigitalChannel.class, "lift_switch");
+        coneDetector = (VirtualRobotController.ColorSensorImpl) hardwareMap.colorSensor.get("cone_detector");
         hardwareMap.setActive(false);
     }
 
     protected void createHardwareMap() {
         super.createHardwareMap();
         hardwareMap.put("claw", new ServoImpl());
-        hardwareMap.put("lift_motor", new DcMotorExImpl(MotorType.Gobilda137));
+        hardwareMap.put("horizontal", new ServoImpl());
+
+        hardwareMap.put("right_lift_motor", new DcMotorExImpl(MotorType.Gobilda137));
+        hardwareMap.put("left_lift_motor", new DcMotorExImpl(MotorType.Gobilda137));
         hardwareMap.put("cone_detector", controller.new ColorSensorImpl());
+        hardwareMap.put("lift_switch", new DigitalChannelImpl());
     }
 
     public synchronized void updateStateAndSensors(double millis) {
